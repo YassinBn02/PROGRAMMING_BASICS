d60d7035d3410587501582eb1438afde71a39f9e
YassinBn02 <yassinbn02@gmail.com>
2024-09-27 12:46:19 +0200

    Conditionals

---
+++ Day 5 assignments/Conditionals .js
@@ -0,0 +1,72 @@
+
+// Feature 1
+function Conditionals(time,day){
+var reward ="";
+if(time==10){
+    reward="Latte";
+}else if(time>10 && time<16){
+    reward="Hot Choclate";
+}else if(time>16 && time<22){
+    if(day=="Wednsday"){
+        reward="Strawberry";
+    }else{
+        reward="Vanilla";
+    }
+}else{
+    reward="sleep";
+}
+return reward;
+
+//Feature 2
+function Hard(time,day){
+    var reward ="";
+    if(time==10){
+        reward="Latte";
+    }else if(time>10 && time<16){
+        reward="Hot Choclate";
+    }else if(time>16 && time<22){
+        if(day=="Wednsday"){
+            reward="Strawberry";
+            var ice=reward;
+        }else{
+            reward="Vanilla";
+            ice=reward;
+        }
+    }else if(time>15 && time<18){
+        if(time%2==0){
+            reward=ice;
+        }else{
+            reward="Hot Choclate";
+        }
+    }else{
+        reward="sleep";
+    }
+    return reward;
+    }
+
+//Feature 3
+function Hard(time,day){
+    var reward ="";
+    var even=["ice cream","cookies","candy"];
+    var odd=["hot choclate","tea","cake"];
+    if(time==10){
+        reward="Latte";
+    }else if(time>15 && time<18){
+        if(time%2==0){
+            reward=even[Math.floor(Math.random() * even.length)];
+        }else{
+            reward=odd[Math.floor(Math.random() * odd.length)];
+        }
+    }else if(time>10 && time<16){
+        reward="Hot Choclate";
+    }else if(time>16 && time<22){
+        if(day=="Wednsday"){
+            reward="Strawberry";
+        }else{
+            reward="Vanilla";
+        }
+    }else{
+        reward="sleep";
+    }
+    return reward;
+}
