be1254e41cc601dbafd87283f86851cc8cc422cf
YassinBn02 <yassinbn02@gmail.com>
2024-09-27 15:15:34 +0200

    ProblemSolving

---
+++ Day 5 assignments/ProblemSolving.js
@@ -0,0 +1,11 @@
+/*A computer application needs to be able to
+identify if a written paragraph is written in First, Second, or Third Person (check out the following
+if you'd like a refresher on First, Second, and Third Person*/
+
+// we could search by looping through the paragraph
+
+/* so we loop through the paragraph an search if we found I or we then 
+it's first ,if we found you then it's second ,but if we found he she it or they then it's third person
+
+
+//the hard part is looping is the search
